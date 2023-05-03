8e50aa204418c6d04d99ede4fd77f4c79e14dee2
yuuki008 <s22ah261@gmail.com>
2023-05-03 10:01:11 +0900

    feat: ハッシュ問題

---
+++ hash_question.py
@@ -0,0 +1,38 @@
+"""
+1. Input: [11, 2, 5, 9, 10, 3], 12 => Output: (2, 10) or None
+2. Input: [11, 2, 5, 9, 10, 3]     => Output: (11, 9) or None ex) 11 + 9 = 2 + 5 + 10 + 3
+"""
+
+from typing import List, Tuple, Optional
+
+def get_pair(numbers: List[int], target: int) -> Optional[Tuple[int, int]]:
+  cache = set()
+  for num in numbers:
+    cache.add(num)
+    val = target - num
+    if val in cache:
+      return val, num
+
+def get_pair_half_sum(numbers: List[int]) -> Optional[Tuple[int, int]]:
+  half_sum, remainder = divmod(sum(numbers), 2)
+  if remainder != 0:
+    return
+
+  cache = set()
+  print(numbers)
+  for num in numbers:
+    cache.add(num)
+    val = half_sum - num
+    if val in cache:
+      return val, num
+
+
+
+
+if __name__ == '__main__':
+  # l = [11, 2, 5, 9, 10, 3]
+  # t = 12
+  # print(get_pair(l, t))
+
+  l = [11, 2, 5, 9, 10, 3]
+  print(get_pair_half_sum(l))
