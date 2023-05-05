c4c314aa7185ae41f247c5ddd174cce3eac71bc1
yuuki008 <s22ah261@gmail.com>
2023-05-05 10:00:43 +0900

    feat: いらないプリント削除

--- quiz/permutation.py
+++ quiz/permutation.py
@@ -6,12 +6,8 @@ def all_perms_v1(elements: List[int]) -> List[List[int]]:
     return [elements]
   else:
     for perm in all_perms_v1(elements[1:]):
-      print('perm     = ', perm)
-      print('elements = ', elements)
       for i in range(len(elements)):
-        print(perm[:i], elements[0:1], perm[i:])
         result.append(perm[:i] + elements[0:1] + perm[i:])
-      print('result   = ', result)
   return result
 
 
