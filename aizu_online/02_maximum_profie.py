7e28e9f522f090b5469f21a1f438beef5b8b809f
yuuki008 <s22ah261@gmail.com>
2023-05-06 19:19:35 +0900

    feat: 02_maximum_profile

--- aizu_online/02_maximum_profie.py
+++ aizu_online/02_maximum_profie.py
@@ -1,13 +1,23 @@
 def main(strings: str) -> int:
   input = [int(i) for i in strings.split(' ')]
   n, r = input[0], input[1:]
-  ans = 0
-  for i in range(n):
-    for j in range(i, n):
-      if ans < r[j] - r[i]:
-        ans = r[j] - r[i]
+  ans = -1e10
+  for i in range(len(r)):
+    for j in range(i + 1, len(r)):
+      ans = max(ans, r[j] - r[i])
   print(ans)
+
+def main_v2(strings: str) -> int:
+  input = [int(i) for i in strings.split(' ')]
+  n, r = input[0], input[1:]
+  print(n, r)
+  min_value = r[0]
+  max_value = -1e10
+  for i in range(1, len(r)):
+    max_value = max(max_value, r[i] - min_value)
+    min_value = min(r[i], min_value)
+  print(max_value)
 
 
 if __name__ == '__main__':
-  main("6 5 3 1 3 4 3")
+  main_v2('20 100 99 95 86 85 76 75 71 70 61 52 48 47')
