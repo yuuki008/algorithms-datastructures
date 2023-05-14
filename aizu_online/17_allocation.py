40bdeef84e6aeabdf20c8a24268bdc619e154f6c
yuuki008 <s22ah261@gmail.com>
2023-05-14 09:20:34 +0900

    feat: allocation

--- aizu_online/17_allocation.py
+++ aizu_online/17_allocation.py
@@ -1,31 +1,30 @@
-def check(w, p, k):
-    i = 0
-    for _ in range(k):
-        s = 0
-        while s + w[i] <= p:
-            s += w[i]
-            i += 1
-            if i == len(w):
-                return len(w)
-    return i
-
-n, k = map(int, input().split())
-w = [int(input()) for _ in range(n)]
-
-right = 100000 * 10000
-left = 0
-while right - left > 1:
-    p = (right + left) // 2
-    if check(w, p, k) >= len(w):
-        right = p
-    else:
-        left = p
-print(right)
-
-
+"""
+https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_D
+"""
 
 
 n = 5
 k = 3
 w = [8, 1, 7, 3, 9]
-allocation(w, k)
+
+def check(p):
+  i = 0
+  for _ in range(k):
+    s = 0
+    while s + w[i] <= p:
+      s += w[i]
+      i += 1
+      if len(w) == i:
+        return i
+  return i
+
+left = 0
+right = 10000 * 100000
+
+while right - left > 1:
+  p = (left + right) // 2
+  if check(p) >= len(w):
+    right = p
+  else:
+    left = p
+print(right)
