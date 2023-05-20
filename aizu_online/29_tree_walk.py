adfeee23a33ef35b97adb60b88a8d1b00221e706
yuuki008 <s22ah261@gmail.com>
2023-05-20 09:49:33 +0900

    feat: tree_walk

--- aizu_online/29_tree_walk.py
+++ aizu_online/29_tree_walk.py
@@ -1,19 +1,38 @@
-n = 9
+n = 4
 A = [
-[3, 4, 0],
-[4, -1, -1],
-[1, -1, -1],
-[2, -1, -1],
-[0, 1, 2]
+[1, 0 ,-1],
+[0, 2 ,-1],
+[2, 3 ,-1],
+[3, -1, -1],
 ]
 d = [{} for _ in range(n)]
 for i in range(len(A)):
   d[A[i][0]]['node'] = A[i][0]
   d[A[i][0]]['left'] = A[i][1]
   d[A[i][0]]['right'] = A[i][2]
+  d[A[i][1]]['parent'] = A[i][0]
+  d[A[i][2]]['parent'] = A[i][0]
+
+def get_max_height_node(A):
+  def _height(node, height):
+    if node == -1:
+      return height
+
+    left_height = _height(d[node]['left'], height + 1)
+    right_height = _height(d[node]['right'], height + 1)
+    return max(left_height, right_height)
+  max_height = 0
+  max_height_node = A[0]
+  for a in A:
+    print(_height(a[0],0))
+    current_node_height = _height(a[0], 0)
+    if current_node_height > max_height:
+      max_height = current_node_height
+      max_height_node = a
+  return max_height_node[0]
 
 pre_order_list = []
-def pre_order(A):
+def pre_order(node):
   def _pre_order(a):
     if a == -1:
       return
@@ -21,10 +40,10 @@ def pre_order(A):
     pre_order_list.append(a)
     _pre_order(d[a]['left'])
     _pre_order(d[a]['right'])
-  _pre_order(d[A[0][0]]['node'])
+  _pre_order(d[node]['node'])
 
 in_order_list = []
-def in_order(A):
+def in_order(node):
   def _in_order(a):
     if a == -1:
       return
@@ -32,10 +51,10 @@ def in_order(A):
     _in_order(d[a]['left'])
     in_order_list.append(a)
     _in_order(d[a]['right'])
-  _in_order(d[A[0][0]]['node'])
+  _in_order(d[node]['node'])
 
 post_order_list = []
-def post_order(A):
+def post_order(node):
   def _post_order(a):
     if a == -1:
       return
@@ -43,18 +62,18 @@ def post_order(A):
     _post_order(d[a]['left'])
     _post_order(d[a]['right'])
     post_order_list.append(a)
-  _post_order(d[A[0][0]]['node'])
+  _post_order(d[node]['node'])
 
-pre_order(A)
-in_order(A)
-post_order(A)
+max_depth_node = get_max_height_node(A)
+pre_order(max_depth_node)
+in_order(max_depth_node)
+post_order(max_depth_node)
 
 print('Preorder')
-print(' ', *pre_order_list, ' ')
 print(''.join(' ' + str(num) for num in pre_order_list))
 
 print('Inorder')
-print(' ', *in_order_list, ' ')
+print(''.join(' ' + str(num) for num in in_order_list))
 
 print('Postorder')
-print(' ', *post_order_list, ' ')
+print(''.join(' ' + str(num) for num in post_order_list))
