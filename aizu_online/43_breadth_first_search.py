b3411210dd0dd204750e0ed8da6a302010f0e63f
yuuki008 <s22ah261@gmail.com>
2023-05-26 21:24:36 +0900

    feat: bfs

---
+++ aizu_online/43_breadth_first_search.py
@@ -0,0 +1,22 @@
+n = 6
+G = [
+[1, 2, 2, 4],
+[2, 1, 5],
+[3, 2, 5, 6],
+[4, 0],
+[5, 1, 4],
+[6, 1, 6],
+]
+
+D = [0, 0] + [-1 for _ in range(n-1)]
+Q = [G[0][0]]
+
+while len(Q):
+  u = Q.pop(0)
+  for v in sorted(G[u-1][2:]):
+    if D[v] == -1:
+      D[v] = D[u] + 1
+      Q.append(v)
+
+for i in range(1, len(D)):
+  print(i, D[i])
